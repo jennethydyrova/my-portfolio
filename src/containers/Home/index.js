d6f2677e3b354ea62a161e5e200feae2b843b40b
jennethydyrova <jennethydyrovaa@gmail.com>
2020-11-25 18:41:22 +0300

    fix styling issues in home

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import HomeMainInfo from "../../components/HomeMainInfo";
 import TechnologyHome from "../../components/Technologies";
-import "./index.scss";
 
 const Home = () => {
   return (
